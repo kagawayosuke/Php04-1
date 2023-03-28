de7287c2ac619cf2b20ee3b5ee8aabb7a570e16b
0924yosuke <dj.yosuke.i.o.u@gmail.com>
2023-03-28 21:41:59 +0900

    修正

--- lesson05.php
+++ lesson05.php
@@ -1,18 +1,19 @@
 ﻿<?php
-// 連想配列の要素name, age, genderに
-// 田中,25,男
-// という値を格納し、
 
-// 田中
+// 連想配列の要素name, age, genderに
+// 田中,25,男
+// という値を格納し、
+// 田中
 // 25歳・男性
 
-// という形で出力してください。
+// という形で出力してください。
 
 $array = [
- 'name'=>['田中'],
- 'age'=>['25'],
- 'gender'=>['男']
+    'name'=>['田中'],
+    'age'=>['25'],
+    'gender'=>['男']
 ];
 
-echo $array['name'][0],"\n", $array['age'][0].'歳・', $array['gender'][0]
+echo $array['name'][0],"\n", $array['age'][0].'歳・',  $array['gender'][0].'性'
+
 ?>
--- lesson09.php
+++ lesson09.php
@@ -1,11 +1,9 @@
 ﻿<?php
-// 引数として数値を渡すと+1して返す関数を作り、
-// 返り値を出力してください。
-function plus($num1, $num2){
- $sum = $num1 + $num2;
- return $sum;
+// 引数として数値を渡すと+1して返す関数を作り、
+// 返り値を出力してください。
+function foo($val){
+  echo $val + 1;
 }
-$sum = plus(0, 1);
-echo $sum
-
+$num = 7;
+foo($num);
 ?>
